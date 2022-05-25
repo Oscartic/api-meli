37168d245e79c74514efb241f033d9403d1e123e
oscartic <orballesteros@falabella.cl>
2022-05-24 22:57:08 -0400

    :pencil2: add getDecimalPart func

--- src/api/v1/helpers/miscellaneous.js
+++ src/api/v1/helpers/miscellaneous.js
@@ -1,7 +1,12 @@
 const miscellaneous = {
-        author: {
-            name: 'Oscar René',
-            lastName: 'Ballesteros Plaza'
+    author: {
+        name: 'Oscar René',
+        lastName: 'Ballesteros Plaza'
+    },
+    getDecimalPart: (num) => {
+        if (Number.isInteger(num)) return 0;
+        const decimalStr = num.toString().split('.')[1];
+        return Number(decimalStr);
     }
 }
 
