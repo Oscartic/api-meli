1d31433228437410c47f10740c99b8d484ddbc7c
oscartic <orballesteros@falabella.cl>
2022-05-25 22:13:23 -0400

    config cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const productRoutes = require('./api/v1/routes/products.routes');
 
 const app = express();
+// * Pending configured Cors 
+// const corsOptions = {
+//     origin: process.env.FRONT_ORIGIN_URL, 
+//     credentials:true,
+//     optionSuccessStatus:200
+// };
 app.use(express.json());
+app.use(cors());
 app.use(morgan('dev'));
 app.use('/api/v1', productRoutes); 
 const PORT = process.env.PORT || 3001;
